73cf439dab17769606ead92e70f78216dd945946
iamdarshangowda <iamdarshangowda@gmail.com>
2022-05-10 21:21:46 +0530

    added url data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,37 @@
 import React from "react";
+import memeData from "../data";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState("");
+
+  function generate() {
+    const urlObj = memeData["data"]["memes"];
+    const image = urlObj[Math.floor(Math.random() * urlObj.length)].url;
+    setMeme(image);
+  }
+
   return (
-    <div className="main">
-      <input
-        type="text"
-        id="top"
-        name="top"
-        placeholder="Top text"
-        className="input"
-      />
-      <input
-        type="text"
-        id="bottom"
-        name="bottom"
-        placeholder="Bottom text"
-        className="input"
-      />
-      <button className="button">Generate Meme 🖼</button>
-    </div>
+    <main>
+      <div className="main">
+        <input
+          type="text"
+          id="top"
+          name="top"
+          placeholder="Top text"
+          className="input"
+        />
+        <input
+          type="text"
+          id="bottom"
+          name="bottom"
+          placeholder="Bottom text"
+          className="input"
+        />
+        <button className="button" onClick={generate}>
+          Generate Meme 🖼
+        </button>
+      </div>
+      <img src={meme} className="memeImage" />
+    </main>
   );
 }
